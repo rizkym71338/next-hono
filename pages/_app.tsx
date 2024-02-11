189d4d9223320d4abb4bcb0d23968c7828a657f5
Rizky Maulana <rizkym71338@gmail.com>
2024-02-11 07:32:00 +0700

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,3 @@
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: any) {
+  return <Component {...pageProps} />;
 }
